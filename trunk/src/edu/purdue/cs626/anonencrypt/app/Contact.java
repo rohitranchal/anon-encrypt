779130d61e09fb2b90a6f208e4f69704601490cc
ruchith.fernando@gmail.com <ruchith.fernando@gmail.com@6be610f6-3184-3a6f-f40d-075a99ebe940>
2011-04-20 05:30:16 +0000

    contact instance and I'm going to get some sleep

---
+++ trunk/src/edu/purdue/cs626/anonencrypt/app/Contact.java
@@ -0,0 +1,58 @@
+package edu.purdue.cs626.anonencrypt.app;
+
+import edu.purdue.cs626.anonencrypt.AEParameters;
+import edu.purdue.cs626.anonencrypt.AEPrivateKey;
+import it.unisa.dia.gas.jpbc.Element;
+
+/**
+ * A remote contact.
+ * 
+ * @author Ruchith Fernando
+ *
+ */
+public class Contact {
+
+	/**
+	 * The identifier issued by the remote contact.
+	 */
+	private Element id;
+	
+	/**
+	 * The global parameters of the remote contact.
+	 */
+	private AEParameters params;
+	
+	/**
+	 * Friendly identifier of the remote contact.
+	 */
+	private String contactId;
+	
+	/**
+	 * Private key issued by the remote contact
+	 */
+	private AEPrivateKey privKey;
+	
+	
+	public Contact() {
+		//TODO
+	}
+
+	public Element getId() {
+		return id;
+	}
+
+	public AEParameters getParams() {
+		return params;
+	}
+
+	public String getContactId() {
+		return contactId;
+	}
+
+	public AEPrivateKey getPrivKey() {
+		return privKey;
+	}
+	
+	
+	
+}

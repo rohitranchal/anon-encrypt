5b5b15987990fbad20fd3a1d5f50a5fee870e962
ruchith.fernando <ruchith.fernando@gmail.com>
2011-04-21 15:13:42 +0000

    Missing test case and build script to make sure I won't miss tests with new changes

---
+++ test/edu/purdue/cs626/anonencrypt/app/TestContactPrivData.java
@@ -0,0 +1,47 @@
+package edu.purdue.cs626.anonencrypt.app;
+
+import java.io.ByteArrayInputStream;
+
+import org.apache.axiom.om.OMElement;
+import org.apache.axiom.om.impl.builder.StAXOMBuilder;
+
+import it.unisa.dia.gas.jpbc.Element;
+import it.unisa.dia.gas.plaf.jpbc.pairing.CurveParams;
+import it.unisa.dia.gas.plaf.jpbc.pairing.a1.TypeA1CurveGenerator;
+import edu.purdue.cs626.anonencrypt.AEParameterGenerator;
+import edu.purdue.cs626.anonencrypt.AEParameters;
+import edu.purdue.cs626.anonencrypt.AEPrivateKey;
+import edu.purdue.cs626.anonencrypt.RootKeyGen;
+import junit.framework.TestCase;
+
+public class TestContactPrivData extends TestCase {
+
+	public void testSerialize() throws Exception {
+		CurveParams curveParams = (CurveParams) new TypeA1CurveGenerator(4, 32)
+				.generate();
+
+		AEParameterGenerator paramGen = new AEParameterGenerator();
+		paramGen.init(curveParams);
+		AEParameters params = paramGen.generateParameters();
+
+		RootKeyGen rkg = new RootKeyGen();
+		rkg.init(params);
+		Element id1 = paramGen.getPairing().getZr().newRandomElement();
+		AEPrivateKey contactPriv = rkg.genKey(id1, paramGen.getMasterKey());
+
+		ContactPrivData cert = new ContactPrivData(params, id1, contactPriv);
+		
+		System.out.println(cert.serialize());
+		
+		
+		OMElement elem = new StAXOMBuilder(new ByteArrayInputStream(cert.serialize().getBytes())).getDocumentElement();
+		ContactPrivData newData = new ContactPrivData(elem);
+		
+		assertEquals(id1, newData.getId());
+		assertEquals(contactPriv.getC1(), newData.getPrivKey().getC1());
+		assertEquals(contactPriv.getC2(), newData.getPrivKey().getC2());
+		assertEquals(contactPriv.getC3(), newData.getPrivKey().getC3());
+		
+	}
+
+}

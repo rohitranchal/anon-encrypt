3bd3c555e2def7814bebbea2c53de6f000cc98cd
ruchith.fernando <ruchith.fernando@gmail.com>
2011-04-22 04:41:11 +0000

    Completed the installer test Installer bug fix Holding on to the pairing in the parameters

--- test/edu/purdue/cs626/anonencrypt/app/TestApplicationInstaller.java
+++ test/edu/purdue/cs626/anonencrypt/app/TestApplicationInstaller.java
@@ -1,10 +1,18 @@
 package edu.purdue.cs626.anonencrypt.app;
 
+import it.unisa.dia.gas.jpbc.Element;
+
 import java.io.File;
+import java.io.FileInputStream;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
+import org.apache.axiom.om.OMElement;
+import org.apache.axiom.om.impl.builder.StAXOMBuilder;
+import org.apache.axiom.om.util.Base64;
+
+import edu.purdue.cs626.anonencrypt.AEParameters;
 import edu.purdue.cs626.anonencrypt.db.Database;
 import junit.framework.TestCase;
 
@@ -19,26 +27,50 @@ public class TestApplicationInstaller extends TestCase {
 		String dbPath = userHome + File.separator
 				+ ApplicationInstaller.CONFIG_DIR + File.separator
 				+ ApplicationInstaller.DB_NAME;
-		
+
 		Connection conn = Database.getConnection(dbPath);
 
 		Statement s = conn.createStatement();
-        
-        s.execute("INSERT INTO Contact " +
-        			"VALUES('Bob', '251789358979577744758182258194692528664', " 
-        			+ "'324683896779935702435841186478040627037', '')");
-        
 
-        ResultSet rs = s.executeQuery("SELECT * FROM Contact");
-        while(rs.next()) {
-        	assertEquals("Bob", rs.getString(1));
-        	assertEquals("251789358979577744758182258194692528664", rs.getString(2));
-        	assertEquals("324683896779935702435841186478040627037", rs.getString(3));
-        }
-        
-        //cleanup
-        installer.unInstall();
+		s.execute("INSERT INTO Contact "
+				+ "VALUES('Bob', '251789358979577744758182258194692528664', "
+				+ "'324683896779935702435841186478040627037', '')");
 
+		ResultSet rs = s.executeQuery("SELECT * FROM Contact");
+		while (rs.next()) {
+			assertEquals("Bob", rs.getString(1));
+			assertEquals("251789358979577744758182258194692528664",
+					rs.getString(2));
+			assertEquals("324683896779935702435841186478040627037",
+					rs.getString(3));
+		}
+
+		String paramPath = userHome + File.separator
+				+ ApplicationInstaller.CONFIG_DIR + File.separator
+				+ ApplicationInstaller.PARAM_FILE_NAME;
+		StAXOMBuilder builder = new StAXOMBuilder(paramPath);
+		OMElement paramElem = builder.getDocumentElement();
+		AEParameters params = new AEParameters(paramElem);
+
+		// Check whether we recover the parameters properly
+		assertEquals(paramElem.toString(), params.serialize());
+
+		String mkPath = userHome + File.separator
+				+ ApplicationInstaller.CONFIG_DIR + File.separator
+				+ ApplicationInstaller.MASTER_KEY_FILE_NAME;
+		File mkFile = new File(mkPath);
+		FileInputStream fis = new FileInputStream(mkFile);
+		byte[] data = new byte[(int)mkFile.length()];
+		fis.read(data, 0, data.length);
+		fis.close();
 		
+		Element elem = params.getPairing().getG1().newElement();
+		elem.setFromBytes(data);
+		
+		assertEquals(Base64.encode(data), Base64.encode(elem.toBytes()));
+		
+		// cleanup
+		installer.unInstall();
+
 	}
 }

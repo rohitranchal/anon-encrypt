71ab3034d3029caf49c032f1e88be32f3fd9c7e1
ruchith.fernando <ruchith.fernando@gmail.com>
2011-04-22 06:16:28 +0000

    register remote contact done

--- test/edu/purdue/cs626/anonencrypt/app/TestApplication.java
+++ test/edu/purdue/cs626/anonencrypt/app/TestApplication.java
@@ -1,9 +1,11 @@
 package edu.purdue.cs626.anonencrypt.app;
 
+import java.io.ByteArrayInputStream;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
+import org.apache.axiom.om.impl.builder.StAXOMBuilder;
 import org.apache.axiom.om.util.Base64;
 
 import edu.purdue.cs626.anonencrypt.db.Database;
@@ -14,46 +16,63 @@ import junit.framework.TestCase;
  * Testing main application API behavior.
  * 
  * @author Ruchith Fernando
- *
+ * 
  */
 public class TestApplication extends TestCase {
 
 	private ApplicationInstaller installer;
-	
+
 	@Override
 	protected void setUp() throws Exception {
-		
+
 		this.installer = new ApplicationInstaller();
 		this.installer.install();
-		
+
 		super.setUp();
 	}
 
 	@Override
 	protected void tearDown() throws Exception {
-		
+
 		this.installer.unInstall();
-		
+
 		super.tearDown();
 	}
 
-	public void testCreateContact() throws Exception {
+	public void testCreateContactAndRegister() throws Exception {
 		Application app = new Application();
 		ContactPrivData data = app.createContact("Bob");
-		
-		//Probe the DB to check the inserted values
+
+		// now register the contact using the same private information
+		app.registerContact("Bob", data);
+
+		// Probe the DB to check the inserted values
 		Connection conn = Database.getConnection();
 		Statement s = conn.createStatement();
 		ResultSet rs = s.executeQuery("SELECT * FROM Contact");
-		String user ="";
+		String user = "";
 		String id = "";
-		while(rs.next()) {
+		String myIDFromContact = "";
+		String privDatafromContact = "";
+
+		while (rs.next()) {
 			user = rs.getString(1);
 			id = rs.getString(2);
+			privDatafromContact = rs.getString(4);
+			myIDFromContact = rs.getString(5);
 		}
-		
+
 		assertEquals("Bob", user);
 		assertEquals(Base64.encode(data.getId().toBytes()), id);
-		
+
+		assertEquals(Base64.encode(data.getId().toBytes()), myIDFromContact);
+
+		StAXOMBuilder builder = new StAXOMBuilder(new ByteArrayInputStream(
+				privDatafromContact.getBytes()));
+
+		ContactPrivData dataFromContact = new ContactPrivData(builder.getDocumentElement());
+		assertEquals(data.getId(), dataFromContact.getId());
+		assertEquals(data.getParams().getCurveParams(), dataFromContact.getParams().getCurveParams());
 	}
+
 }

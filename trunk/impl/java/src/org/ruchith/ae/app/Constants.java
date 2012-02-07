bc24cd726518d36eb14e5389ade2628f4b0eb6da
ruchith.fernando@gmail.com <ruchith.fernando@gmail.com@6be610f6-3184-3a6f-f40d-075a99ebe940>
2012-02-07 19:38:20 +0000

    refactor

---
+++ trunk/impl/java/src/org/ruchith/ae/app/Constants.java
@@ -0,0 +1,10 @@
+package org.ruchith.ae.app;
+
+public class Constants {
+
+	public static final String PARAM_FILE_NAME = "parameters.xml";
+	public static final String CONFIG_DIR = ".ae";
+	public static final String DB_NAME = "db";
+	public static final String MASTER_KEY_FILE_NAME = "mk";
+
+}

d6a6927d802de750d4d91dbffcf33732020a7c0c
ruchith.fernando <ruchith.fernando@gmail.com>
2012-02-07 19:38:20 +0000

    refactor

---
+++ impl/java/src/org/ruchith/ae/app/Application.java
@@ -0,0 +1,407 @@
+package org.ruchith.ae.app;
+
+import it.unisa.dia.gas.jpbc.Element;
+import it.unisa.dia.gas.jpbc.Field;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.apache.axiom.om.OMElement;
+import org.apache.axiom.om.impl.builder.StAXOMBuilder;
+import org.apache.axiom.om.util.Base64;
+import org.ruchith.ae.app.db.Database;
+import org.ruchith.ae.base.AECipherText;
+import org.ruchith.ae.base.AEParameters;
+import org.ruchith.ae.base.AEPrivateKey;
+import org.ruchith.ae.base.ContactKeyGen;
+import org.ruchith.ae.base.Decrypt;
+import org.ruchith.ae.base.Encrypt;
+import org.ruchith.ae.base.ReKey;
+import org.ruchith.ae.base.ReKeyInformation;
+import org.ruchith.ae.base.RootKeyGen;
+import org.ruchith.ae.base.TextEncoder;
+import org.ruchith.ae.base.Util;
+
+
+/**
+ * Main application API that depends on an installation. (i.e. requires the
+ * configuration to be in the user home directory)
+ * 
+ * @author Ruchith Fernando
+ * 
+ */
+public class Application {
+
+	/**
+	 * My encryption parameters.
+	 */
+	private AEParameters params;
+
+	/**
+	 * My public key
+	 */
+	private Element masterKey;
+
+	/**
+	 * This holds the temporary private keys created in requesting a contact's
+	 * updates.
+	 */
+	private HashMap<String, AEPrivateKey> tmpPrivKeyMap = new HashMap<String, AEPrivateKey>();
+
+	private HashMap<String, AEParameters> paramCache = new HashMap<String, AEParameters>();
+
+	public Application() throws Exception {
+		String userHome = System.getProperty("user.home");
+		String paramPath = userHome + File.separator + Constants.CONFIG_DIR
+				+ File.separator + Constants.PARAM_FILE_NAME;
+		StAXOMBuilder builder = new StAXOMBuilder(paramPath);
+		OMElement paramElem = builder.getDocumentElement();
+		this.params = new AEParameters(paramElem);
+
+		String mkPath = userHome + File.separator + Constants.CONFIG_DIR
+				+ File.separator + Constants.MASTER_KEY_FILE_NAME;
+		File mkFile = new File(mkPath);
+		FileInputStream fis = new FileInputStream(mkFile);
+		byte[] data = new byte[(int) mkFile.length()];
+		fis.read(data, 0, data.length);
+		fis.close();
+
+		Element elem = params.getPairing().getG1().newElement();
+		elem.setFromBytes(data);
+		this.masterKey = elem.getImmutable();
+
+	}
+
+	/**
+	 * Create a new random private key for the contact identified by the given
+	 * friendly name and return the data to be sent to that contact.
+	 * 
+	 * @param name
+	 *            Friendly name of the contact as a {@link String} value.
+	 * @return A {@link ContactPrivData} object. Simply call
+	 *         {@link ContactPrivData#serialize()} to obtain the XML data to
+	 *         send to the contact.
+	 */
+	public ContactPrivData createContact(String name) throws Exception {
+
+		// Generate a key with a random ID
+		RootKeyGen rkg = new RootKeyGen();
+		rkg.init(this.params);
+		Field zr = this.params.getPairing().getZr();
+		Element id1 = zr.newRandomElement().getImmutable();
+		Element r = zr.newRandomElement().getImmutable();
+		AEPrivateKey contactKey = rkg.genKey(id1, this.masterKey, r);
+
+		// Store in the database
+		String sql = "INSERT INTO Contact(contactId, id, random) VALUES ("
+				+ "'" + name + "'," + "'" + Base64.encode(id1.toBytes()) + "',"
+				+ "'" + Base64.encode(r.toBytes()) + "')";
+
+		Connection conn = Database.getConnection();
+		Statement s = conn.createStatement();
+		s.execute(sql);
+		return new ContactPrivData(this.params, id1, contactKey);
+	}
+
+	/**
+	 * Register a remote contact with the private information he/she created for
+	 * me.
+	 * 
+	 * @param name
+	 *            Friendly name of the remote contact.
+	 * @param data
+	 *            {@link ContactPrivData} instance from the remote contact.
+	 */
+	public void registerContact(String name, ContactPrivData data)
+			throws Exception {
+		// Store in the database
+		String sql = "UPDATE Contact SET privDataFromContact='"
+				+ data.serialize() + "', myIDFromContact='"
+				+ Base64.encode(data.getId().toBytes())
+				+ "' WHERE contactId = '" + name + "'";
+		Connection conn = Database.getConnection();
+		Statement s = conn.createStatement();
+		s.execute(sql);
+	}
+
+	public boolean removeContact(String name) throws Exception {
+		String sql = "DELETE FROM Contact WHERE contactId = '" + name + "'";
+		Connection conn = Database.getConnection();
+		Statement s = conn.createStatement();
+		s.execute(sql);
+		return s.getUpdateCount() == 1;
+	}
+	
+	/**
+	 * Re-key parameters and return the information to publish.
+	 * 
+	 * @return Public information as a {@link ReKeyInformation} instance.
+	 * @throws Exception
+	 */
+	public ReKeyInformation reKey() throws Exception {
+
+		ReKey reKey = new ReKey(this.params);
+		Element mk = reKey.update();
+		ApplicationInstaller.saveMasterKey(mk);
+
+		// Get all contacts in the database and generate public information.
+		String sql = "SELECT id, random FROM Contact";
+		Connection conn = Database.getConnection();
+		Statement s = conn.createStatement();
+
+		ResultSet rs = s.executeQuery(sql);
+		HashMap<Element, Element> idRndMap = new HashMap<Element, Element>();
+		while (rs.next()) {
+			Element id = this.params.getPairing().getZr().newElement();
+			Element rnd = this.params.getPairing().getZr().newElement();
+
+			id.setFromBytes(Base64.decode(rs.getString(1)));
+			rnd.setFromBytes(Base64.decode(rs.getString(2)));
+
+			idRndMap.put(id, rnd);
+		}
+
+		return reKey.getPublicInfo(idRndMap);
+
+	}
+	
+	public boolean processReKey(String user, String reKeyData) throws Exception {
+		//Obtian user's parameters
+		ContactPrivData cpd = this.getContactPrivData(user);
+		
+		ReKeyInformation rki = new ReKeyInformation(
+				Util.getOMElement(reKeyData), cpd.getParams().getPairing());
+		
+		Element rnd = rki.getRnd();
+		
+		//Raise the given id to rnd and obtain the search key 
+		Element searchKey = rnd.powZn(cpd.getId().getImmutable()).getImmutable();
+		
+		Element tmp = rki.getNewC1map().get(Base64.encode(searchKey.toBytes()));
+		if(tmp != null) {
+			Element c1 = tmp.getImmutable();
+			AEPrivateKey privKey = cpd.getPrivKey();
+			AEPrivateKey newPrivKey = new AEPrivateKey(c1, privKey.getC2(), privKey.getC3());
+			
+			AEParameters params = cpd.getParams();
+			params.setG1(rki.getG1().getImmutable()); //update g1
+			
+			cpd = new ContactPrivData(params, cpd.getId(), newPrivKey);
+			this.registerContact(user, cpd);
+			
+			return true;
+		} else {
+			//Too bad you have been revoked by the user!!!
+			return false;
+		}
+		
+	}
+	
+	/**
+	 * Store message from a user
+	 * @param user The user name
+	 * @param msg Message
+	 * @throws Exception
+	 */
+	public void saveMessage(String user, String msg) throws Exception {
+		String sql = "UPDATE Contact SET lastMsg='" + msg
+				+ "' WHERE contactId = '" + user + "'";
+		Connection conn = Database.getConnection();
+		Statement s = conn.createStatement();
+		s.execute(sql);
+	}
+	
+	public String getMessage(String user) throws Exception {
+		String sql = "SELECT lastMsg FROM Contact WHERE contactId = '" + user + "'";
+		Connection conn = Database.getConnection();
+		Statement s = conn.createStatement();
+		ResultSet rs = s.executeQuery(sql);
+		rs.next();
+		return rs.getString(1);
+	}
+
+	/**
+	 * Send the last message of the requested user.
+	 * 
+	 * @param updateRequest
+	 * @return
+	 */
+	public String getUpdate(String updateRequest) throws Exception {
+		UpdateRequest ur = new UpdateRequest(Util.getOMElement(updateRequest));
+
+		// Get the user's parameter's from the DB
+		String user = ur.getUser();
+
+		Connection conn = Database.getConnection();
+		Statement s = conn.createStatement();
+		String sql = "SELECT privDataFromContact, lastMsg FROM Contact WHERE contactId='"
+				+ user + "'";
+		ResultSet rs = s.executeQuery(sql);
+		if (rs.next()) {
+			String privDataStr = rs.getString(1);
+			String msg = rs.getString(2);
+			
+			if(msg == null) {
+				return null;
+			}
+
+			// Create priv data object
+			ContactPrivData cpd = new ContactPrivData(
+					Util.getOMElement(privDataStr));
+			AEParameters contactParams = cpd.getParams();
+
+			// Convert the request id to an Element
+			Element idElem = contactParams.getPairing().getG1().newElement();
+			idElem.setFromBytes(Base64.decode(ur.getRndId()));
+			idElem = idElem.getImmutable();
+
+			// Encode the msg to be sent
+			TextEncoder encoder = new TextEncoder();
+			encoder.init(contactParams);
+			Element[] msgElems = encoder.encode(msg);
+
+			// Encrypt the msg
+			Encrypt encrypt = new Encrypt();
+			encrypt.init(contactParams);
+
+			AECipherText cipherText = encrypt.doEncrypt(msgElems, idElem);
+
+			UpdateResponse uRes = new UpdateResponse(user,
+					cipherText.serialize());
+
+			return uRes.serialize();
+
+		}
+
+		return null;
+
+	}
+
+	/**
+	 * Create an update request for a given user.
+	 * 
+	 * @param user
+	 *            The name of the user as a {@link String}.
+	 * 
+	 * @return An {@link UpdateRequest} object with the new random key.
+	 */
+	public UpdateRequest getUpdateRequest(String user) throws Exception {
+
+		Connection conn = Database.getConnection();
+		Statement s = conn.createStatement();
+		String sql = "SELECT privDataFromContact FROM Contact WHERE contactId='"
+				+ user + "'";
+		ResultSet rs = s.executeQuery(sql);
+		if (rs.next()) {
+			String privDataStr = rs.getString(1);
+
+			// Create priv data object
+			ContactPrivData cpd = new ContactPrivData(
+					Util.getOMElement(privDataStr));
+
+			ContactKeyGen keyGen = new ContactKeyGen();
+			keyGen.init(cpd.getId(), cpd.getPrivKey(), cpd.getParams());
+
+			// Random identifier
+			Element rndId = keyGen.genRandomID();
+
+			// Store the private key in memory
+			AEPrivateKey privKey = keyGen.getTmpPrivKey(rndId);
+			this.tmpPrivKeyMap.put(user, privKey);
+			this.paramCache.put(user, cpd.getParams());
+
+			// The temp public key
+			Element tmpPubKey = keyGen.getTmpPubKey(rndId);
+
+			// Return a new reqeuest instance
+			return new UpdateRequest(user, Base64.encode(tmpPubKey.toBytes()));
+
+		}
+
+		// No such user -- This won't be reached!
+		return null;
+	}
+
+	public String processUpdateResponse(String response) throws Exception {
+		UpdateResponse ur = new UpdateResponse(Util.getOMElement(response));
+
+		String user = ur.getUser();
+
+		AEPrivateKey privKey = this.tmpPrivKeyMap.get(user);
+		AEParameters contactParams = this.paramCache.get(user);
+		
+		if(privKey == null) {
+			throw new RuntimeException("No private key");
+		}
+
+		if(contactParams == null) {
+			throw new RuntimeException("Contact parameters");
+		}
+		
+		String cipherTxtStr = ur.getCipherTextString();
+		AECipherText ct = new AECipherText(Util.getOMElement(cipherTxtStr),
+				contactParams.getPairing());
+
+		Decrypt decrypt = new Decrypt();
+		decrypt.init(contactParams);
+		Element[] plainElems = decrypt.doDecrypt(ct.getBlocks(), privKey);
+		TextEncoder encoder = new TextEncoder();
+		encoder.init(contactParams);
+		byte[] decoded = encoder.decode(plainElems);
+		
+		String msg = new String(decoded).trim();
+		try {
+			this.saveMessage(user, msg);
+		} catch (Exception e) {
+			msg += " << This was not saved!";
+		}
+		
+		return msg;
+	}
+
+	public String[] getContactList() throws Exception {
+		Connection conn = Database.getConnection();
+		Statement s = conn.createStatement();
+		String sql = "SELECT contactId FROM Contact";
+		
+		ArrayList<String> names = new ArrayList<String>();
+		ResultSet rs = s.executeQuery(sql);
+		while(rs.next()) {
+			names.add(rs.getString(1));
+		}
+
+		return names.toArray(new String[names.size()]);
+	}
+	
+	
+	private ContactPrivData getContactPrivData(String user) throws Exception {
+		Connection conn = Database.getConnection();
+		Statement s = conn.createStatement();
+		String sql = "SELECT privDataFromContact FROM Contact WHERE contactId='"
+				+ user + "'";
+		ResultSet rs = s.executeQuery(sql);
+		if (rs.next()) {
+			String privDataStr = rs.getString(1);
+			// Create priv data object
+			return  new ContactPrivData(Util.getOMElement(privDataStr));
+			
+		}
+		
+		return null; //No such user
+	}
+	
+	
+	
+	/*
+	 * For test cases
+	 */
+	AEParameters getParams() {
+		return params;
+	}
+
+}
---
+++ impl/java/src/org/ruchith/ae/app/demo/Messenger.java
@@ -0,0 +1,278 @@
+package org.ruchith.ae.app.demo;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.ruchith.ae.app.Application;
+import org.ruchith.ae.app.ApplicationInstaller;
+import org.ruchith.ae.app.ContactPrivData;
+import org.ruchith.ae.app.UpdateRequest;
+import org.ruchith.ae.base.ReKeyInformation;
+import org.ruchith.ae.base.Util;
+
+
+public class Messenger {
+
+	private static Application app;
+
+	private static void printUsage() {
+		System.out.println("Options (Help 0):");
+
+		System.out.println("1.) List contacts");
+		System.out.println("2.) Add contact");
+		System.out.println("3.) Add contact : enter remote priv data");
+		System.out.println("4.) Set direct update of contact");
+		System.out.println("5.) Request remote update of contact");
+		System.out.println("6.) Serve update request");
+		System.out.println("7.) Process remote update of contact");
+		System.out.println("8.) Remove contact");
+		System.out.println("9.) Re-key system");
+		System.out.println("10.) Process contact re-key information");
+		System.out.println("20.) Install");
+		System.out.println("21.) Uninstall");
+		System.out.println("22.) Exit!!!");
+	}
+
+	public static void main(String[] args) throws Exception {
+
+		if (ApplicationInstaller.isInstalled()) {
+			app = new Application();
+		}
+		boolean start = true;
+		int selection = 0;
+		while (selection != 22) {
+			if (start) {
+				printUsage();
+				start = false;
+			}
+			System.out.println("Enter option: ");
+
+			BufferedReader stdin = new BufferedReader(new InputStreamReader(
+					System.in));
+			String value;
+			value = stdin.readLine();
+
+			if (value.equals("")) {
+				continue;
+			}
+
+			selection = Integer.parseInt(value);
+			switch (selection) {
+			case 1:
+				printContactList();
+				break;
+			case 2:
+				addContact(stdin);
+				break;
+			case 3:
+				processRemotePrivData(stdin);
+				break;
+			case 4:
+				doDirectMsg(stdin);
+				break;
+			case 5:
+				remoteUpdateReq(stdin);
+				break;
+			case 6:
+				serveUpdateReq(stdin);
+				break;
+			case 7:
+				doProcessUpdateResponse(stdin);
+				break;
+			case 8:
+				removeContact(stdin);
+				break;
+			case 9:
+				doReKey(stdin);
+				break;
+			case 10:
+				processContactReKey(stdin);
+				break;
+			case 20:
+				install();
+				break;
+			case 21:
+				unInstall();
+				break;
+			case 0:
+				printUsage();
+				break;
+			case 22:
+				exit();
+				break;
+			default:
+				break;
+			}
+		}
+
+	}
+
+	private static void processContactReKey(BufferedReader stdin) throws Exception {
+		System.out.println("Enter contact name :");
+		String user = stdin.readLine();
+		
+		String data = getInputFromZenity("Enter re-key information from " + user);
+		boolean result = app.processReKey(user, data);
+		if(result) {
+			System.out.println("Update successful!");
+		} else {
+			System.out.println("Seems like " + user + 
+					" doesn't consider you to be a friend anymore :P");
+		}
+	}
+
+	private static void doReKey(BufferedReader stdin) throws Exception {
+		System.out.println("Are you sure (Y/N)?");
+		String resp = stdin.readLine();
+		if(resp.equalsIgnoreCase("Y")) {
+			ReKeyInformation rki = app.reKey();
+			System.out.println("--------------RE-KEY_INFO:START--------------");
+			String info = rki.serialize();
+			System.out.println(info);
+			System.out.println("--------------RE-KEY_INFO:END----------------");
+		}
+	}
+
+	private static void removeContact(BufferedReader stdin) throws IOException,
+			Exception {
+		System.out.println("Enter contact name :");
+		String user = stdin.readLine();
+		boolean result = app.removeContact(user);
+		if (result) {
+			System.out.println(user + " removed successfully!");
+		}
+	}
+
+	private static void doProcessUpdateResponse(BufferedReader stdin)
+			throws IOException, FileNotFoundException, Exception {
+		String data = getInputFromZenity("Enter update response");
+		String msg = app.processUpdateResponse(data);
+
+		System.out.println("Update message : " + msg);
+	}
+
+	private static void serveUpdateReq(BufferedReader stdin)
+			throws IOException, FileNotFoundException, Exception {
+		System.out.println("Enter update request : ");
+	
+
+		String data = getInputFromZenity("Enter update request");
+		String resp = app.getUpdate(data);
+		System.out
+				.println("-----------REMOTE_UPDATE_RESPONSE:START-----------");
+		System.out.println(resp);
+		System.out
+				.println("-----------REMOTE_UPDATE_RESPONSE:END-------------");
+	}
+
+	private static void remoteUpdateReq(BufferedReader stdin)
+			throws IOException, Exception {
+		System.out.println("Enter contact name :");
+		String user = stdin.readLine();
+
+		UpdateRequest ur = app.getUpdateRequest(user);
+
+		System.out.println("-----------REMOTE_UPDATE_REQUEST:START-----------");
+		System.out.println(ur.serialize());
+		System.out.println("-----------REMOTE_UPDATE_REQUEST:END-------------");
+	}
+
+	private static void install() throws Exception {
+		if (app != null) {
+			System.out.println("Aleady installed");
+		} else {
+			new ApplicationInstaller().install();
+			app = new Application();
+			System.out.println("Installation complete!");
+		}
+	}
+
+	private static void exit() {
+		System.out.println("BYE!");
+		System.exit(0);
+	}
+
+	private static void unInstall() {
+		if (app != null) {
+			new ApplicationInstaller().unInstall();
+			System.out.println("Removed!");
+			app = null;
+		} else {
+			printInstallMsg();
+		}
+	}
+
+	private static void processRemotePrivData(BufferedReader stdin)
+			throws IOException, FileNotFoundException, Exception {
+		System.out.println("Enter contact name :");
+		String user = stdin.readLine();
+
+		String data = getInputFromZenity("Enter private cert from " + user);
+		app.registerContact(user, new ContactPrivData(Util.getOMElement(data)));
+		System.out.println("Private certificate installed successfully!\n");
+	}
+
+	private static void doDirectMsg(BufferedReader stdin) throws IOException,
+			Exception {
+		System.out.println("Enter contact name:");
+		String user = stdin.readLine();
+		System.out.println("Enter plain message that you received from " + user + " :");
+		String msg = stdin.readLine();
+		app.saveMessage(user, msg);
+		System.out.println(user + " >> " + msg);
+	}
+
+	private static void printContactList() throws Exception {
+		if (app == null) {
+			printInstallMsg();
+		}
+		String[] contacts = app.getContactList();
+		System.out.println("========================================");
+		System.out.println("|          CONTACT LIST                |");
+		System.out.println("========================================");
+
+		for (int i = 0; i < contacts.length; i++) {
+			System.out.println(contacts[i] + " >> "
+					+ app.getMessage(contacts[i]));
+		}
+		System.out.println("========================================");
+
+	}
+
+	private static void addContact(BufferedReader stdin) throws IOException,
+			Exception {
+		if (app == null) {
+			printInstallMsg();
+		}
+		System.out.println("Enter contact name: ");
+		System.out.flush();
+		String name = stdin.readLine();
+		ContactPrivData privData = app.createContact(name);
+		System.out.println("---------------PRIVATE_DATA:START---------------");
+		System.out.println(privData.serialize());
+		System.out.println("---------------PRIVATE_DATA:END-----------------");
+	}
+
+	private static void printInstallMsg() {
+		System.out.println("Please install application!");
+	}
+	
+	private static String getInputFromZenity(String title) throws Exception {
+		String val = "";
+		String[] cmd = new String[]{"zenity", "--text-info", "--editable", "--title=" + title + ""};
+		
+		Process p = Runtime.getRuntime().exec(cmd);
+		BufferedReader in
+		   = new BufferedReader(new InputStreamReader(p.getInputStream()));
+		String data = null;
+		while((data = in.readLine()) != null) {
+			val += data + "\n";
+		}
+		System.out.println(val);
+		return val;
+	}
+	
+	
+}

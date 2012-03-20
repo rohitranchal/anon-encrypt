1590496d7857949ad70ade01dd722f8f9c1c953a
ruchith.fernando@gmail.com <ruchith.fernando@gmail.com@6be610f6-3184-3a6f-f40d-075a99ebe940>
2012-03-20 16:05:03 +0000

    Adding initial code

---
+++ trunk/impl/android/SecMsg/src/org/ruchith/secmsg/AEManager.java
@@ -0,0 +1,80 @@
+package org.ruchith.secmsg;
+
+import it.unisa.dia.gas.jpbc.Element;
+import it.unisa.dia.gas.plaf.jpbc.pairing.CurveParams;
+import it.unisa.dia.gas.plaf.jpbc.pairing.a1.TypeA1CurveGenerator;
+
+import org.bouncycastle.util.encoders.Base64;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.node.ObjectNode;
+import org.ruchith.ae.base.AEParameterGenerator;
+import org.ruchith.ae.base.AEParameters;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.util.Log;
+
+public class AEManager {
+
+	private static final String TAG = "AEManager";
+	
+	private Context mCtx;
+	private DBAdapter mDbHelper;
+	private AEParameters params;
+	private Element masterKey;
+
+	public AEManager(Context ctx, DBAdapter db) {
+		this.mCtx = ctx;
+		this.mDbHelper = db;
+		//Load configuration
+		this.loadConfiguration();
+	}
+
+	private void loadConfiguration() {
+		//Check whether parameters are available 
+		Cursor c = this.mDbHelper.fetchConfig();
+		if(c.getCount() > 0) {
+			c.moveToFirst();
+			String paramVal = c.getString(c.getColumnIndexOrThrow(DBAdapter.KEY_PARAMS));
+			String mkVal = c.getString(c.getColumnIndexOrThrow(DBAdapter.KEY_MASTER_KEY));
+			
+			try {
+				ObjectMapper mapper = new ObjectMapper();
+				ObjectNode on = (ObjectNode) mapper.readTree(paramVal);
+				this.params = new AEParameters(on);
+				
+				Element tmp = this.params.getPairing().getG1().newElement();
+				
+				tmp.setFromBytes(Base64.decode(mkVal));
+				this.masterKey = tmp.getImmutable();
+				
+			} catch (Exception e) {
+				Log.e(TAG, e.getMessage());
+			}
+			
+		} else {
+			//install
+			install();
+		}
+	}
+	
+	private void install() {
+		CurveParams curveParams = (CurveParams) new TypeA1CurveGenerator(4, 32)
+				.generate();
+
+		AEParameterGenerator paramGen = new AEParameterGenerator();
+		paramGen.init(curveParams);
+
+		this.params = paramGen.generateParameters();
+		this.masterKey = paramGen.getMasterKey();
+		
+		//Add to DB
+		String paramVal = this.params.serializeJSON().toString();
+		String mkVal = new String(Base64.encode(this.masterKey.toBytes()));
+		
+		this.mDbHelper.addConfig(paramVal, mkVal);
+		
+		
+	}
+
+}

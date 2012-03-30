cdf0eb9c681435798c3a580108ab5ab06f825334
ruchith.fernando <ruchith.fernando@gmail.com>
2012-03-30 21:54:38 +0000

    entry point to add an entry

--- impl/pubchannel/server/app.js
+++ impl/pubchannel/server/app.js
@@ -30,7 +30,9 @@ app.configure('production', function(){
 // Routes
 
 app.get('/', routes.index);
-app.get('add/', routes.add);
+app.get('/add/', function(req, res) {
+	 res.send('success');
+});
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);

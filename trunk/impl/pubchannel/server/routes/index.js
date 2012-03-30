57b727318597c9be30d71a9e3074607c36107a4a
ruchith.fernando@gmail.com <ruchith.fernando@gmail.com@6be610f6-3184-3a6f-f40d-075a99ebe940>
2012-03-30 22:10:07 +0000

    testing db

--- trunk/impl/pubchannel/server/routes/index.js
+++ trunk/impl/pubchannel/server/routes/index.js
@@ -1,12 +1,17 @@
+var db = require('../db');
 
 /*
  * GET home page.
  */
-
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
 
+/*
+ * Handle incoming entry requests
+ */
 exports.add = function(req, res){
-	  res.send('test');
+	db.addEntry('testing');  
+	res.send('test');
+	  
 };

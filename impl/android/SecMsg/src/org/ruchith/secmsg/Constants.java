20d13ab10a796e8e8c22d2732f23d7a30cf348ba
ruchith.fernando <ruchith.fernando@gmail.com>
2012-04-01 22:22:50 +0000

    sending the message to the pubchannel

---
+++ impl/android/SecMsg/src/org/ruchith/secmsg/Constants.java
@@ -0,0 +1,7 @@
+package org.ruchith.secmsg;
+
+public class Constants {
+
+	public final static String PUBCHANNEL_URL = "http://ruchith.net";
+	public final static String PUBCHANNEL_NEW_ENTRY_URL = PUBCHANNEL_URL + "/add/";
+}

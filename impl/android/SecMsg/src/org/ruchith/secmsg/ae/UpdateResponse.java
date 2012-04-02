9db8ec0b6e2ad89e3f6df7a0be062379ea93b55c
ruchith.fernando <ruchith.fernando@gmail.com>
2012-04-02 22:12:15 +0000

    UpdateResponse wrapper

--- impl/android/SecMsg/src/org/ruchith/secmsg/ae/UpdateResponse.java
+++ impl/android/SecMsg/src/org/ruchith/secmsg/ae/UpdateResponse.java
@@ -1,13 +1,52 @@
 package org.ruchith.secmsg.ae;
 
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.node.ObjectNode;
 
 public class UpdateResponse {
 
 	public static final String TYPE = "update_response";
 	
+	private String replyTo;
+	private String encryptedData;
+	private String encryptedKey;
+	
 	public UpdateResponse(ObjectNode on) {
-		//TODO
+		this.replyTo = on.get("reply_to").getTextValue();
+		this.encryptedData = on.get("encrypted_data").getTextValue();
+		this.encryptedKey = on.get("encrypted_key").getTextValue();
+	}
+	
+	public UpdateResponse(String replyTo, String cipherData, String encryptedKey) {
+		this.replyTo = replyTo;
+		this.encryptedData = cipherData;
+		this.encryptedKey = encryptedKey;
+	}
+	
+	public ObjectNode serializeJSON() {
+		ObjectMapper mapper = new ObjectMapper();
+		JsonNode rootNode = mapper.createObjectNode();
+		ObjectNode on = (ObjectNode) rootNode;
+		
+		on.put("type",  TYPE);
+		on.put("reply_to", this.replyTo);
+		on.put("encrypted_data", this.encryptedData);
+		on.put("encrypted_key", this.encryptedKey);
+	
+		return on;
 	}
 
+	public String getReplyTo() {
+		return replyTo;
+	}
+
+	public String getCipherData() {
+		return encryptedData;
+	}
+
+	public String getEncryptedKey() {
+		return encryptedKey;
+	}
+	
 }

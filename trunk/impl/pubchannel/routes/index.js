f727b5942eb54182ddf3a66282d5828ae7223044
ruchith.fernando@gmail.com <ruchith.fernando@gmail.com@6be610f6-3184-3a6f-f40d-075a99ebe940>
2014-01-02 16:57:28 +0000

    Basic operations

--- trunk/impl/pubchannel/routes/index.js
+++ trunk/impl/pubchannel/routes/index.js
@@ -1,8 +1,38 @@
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+	host     : 'localhost',
+	user     : 'root',
+	password : '',
+	database : 'pubchannel',
+	multipleStatements: true
+});
+connection.connect();
 
-/*
- * GET home page.
- */
+exports.get_all_messages = function(req, res) {
+	var sql = 'SELECT * FROM Message';
+	connection.query(sql, function(err, rows, fields) {
+		if (err) throw err;
+		res.send(rows);
+	});
+}
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
-};
+exports.get_all_messages_after = function(req, res) {
+	var msg_id = req.query.msg_id;
+	var sql = 'SELECT * FROM Message WHERE id > ' + msg_id;
+	connection.query(sql, function(err, rows, fields) {
+		if (err) throw err;
+		res.send(rows);
+	});
+}
+
+exports.add_message = function(req, res) {
+	var msg = req.query.msg;
+	if(typeof msg == 'undefined') {
+		msg = req.body.msg;
+	}
+
+	connection.query("INSERT INTO Message(data) VALUES ('" + msg + "')", function(err, rows, fields) {
+		if (err) throw err;
+		res.send('OK');
+	});
+}

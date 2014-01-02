580a3512468d6fede3b468198e5638a3036154fe
ruchith.fernando@gmail.com <ruchith.fernando@gmail.com@6be610f6-3184-3a6f-f40d-075a99ebe940>
2014-01-02 21:25:06 +0000

    Peers can accept direct messages and generate message requests

--- trunk/impl/base/src/main/java/org/ruchith/ae/peer/Peer.java
+++ trunk/impl/base/src/main/java/org/ruchith/ae/peer/Peer.java
@@ -1,15 +1,18 @@
 package org.ruchith.ae.peer;
 
-import java.util.HashMap;
-
 import it.unisa.dia.gas.jpbc.Element;
 import it.unisa.dia.gas.jpbc.Field;
 import it.unisa.dia.gas.plaf.jpbc.pairing.CurveParams;
 import it.unisa.dia.gas.plaf.jpbc.pairing.a1.TypeA1CurveGenerator;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.bouncycastle.util.encoders.Base64;
 import org.ruchith.ae.base.AEParameterGenerator;
 import org.ruchith.ae.base.AEParameters;
 import org.ruchith.ae.base.AEPrivateKey;
+import org.ruchith.ae.base.ContactKeyGen;
 import org.ruchith.ae.base.RootKeyGen;
 
 /**
@@ -23,6 +26,10 @@ public class Peer {
 	private Element masterKey;
 	
 	private HashMap<String, Contact> contacts = new HashMap<>();
+	private HashMap<String, ContactPrivateData> privData = new HashMap<>();
+	private HashMap<String, ArrayList<String>> messages = new HashMap<>();
+	
+	private HashMap<String, HashMap<Element, AEPrivateKey>> tmpKeyList = new HashMap<>(); 
 	
 	public Peer() {
 		CurveParams curveParams = (CurveParams) new TypeA1CurveGenerator(4, 32)
@@ -61,6 +68,52 @@ public class Peer {
 		return new ContactPrivateData(id1, contactKey, params);
 	}
 
-	
 
+	/**
+	 * Add information provided by a remote contact.
+	 * @param name
+	 * @param privateData
+	 */
+	public void registerContact(String name, ContactPrivateData privateData) {
+		this.privData.put(name, privateData);
+	}
+
+
+	/**
+	 * Add a message directly received from a peer.
+	 * @param user Name of the peer.
+	 * @param message The message.
+	 */
+	public void addDirectMessage(String user, String message) {
+		ArrayList<String> msgs = this.messages.get(user);
+		if(msgs == null) {
+			msgs = new ArrayList<String>();
+			this.messages.put(user, msgs);
+		}
+		
+		msgs.add(message);
+	}
+	
+	public MessageRequest generateRequest(String user) {
+		ContactPrivateData cpd = privData.get(user);
+		
+		ContactKeyGen keyGen = new ContactKeyGen();
+		keyGen.init(cpd.getId(), cpd.getKey(), cpd.getParams());
+		
+		Element rndId = keyGen.genRandomID();
+		AEPrivateKey privKey = keyGen.getTmpPrivKey(rndId);
+		Element tmpPubKey = keyGen.getTmpPubKey(rndId);
+
+		//Store private key
+		HashMap<Element, AEPrivateKey> tmpKeyMap = this.tmpKeyList.get(user);
+		if(tmpKeyMap == null) {
+			tmpKeyMap = new HashMap<Element, AEPrivateKey>();
+		}
+		tmpKeyMap.put(tmpPubKey, privKey);
+		
+		String tmpPubKeyStr = new String(Base64.encode(tmpPubKey.toBytes()));
+		
+		return new MessageRequest(user, tmpPubKeyStr);
+	}
+	
 }
--- trunk/impl/base/src/test/java/org/ruchith/ae/peer/TestPeer.java
+++ trunk/impl/base/src/test/java/org/ruchith/ae/peer/TestPeer.java
@@ -10,9 +10,29 @@ public class TestPeer extends TestCase {
 		Peer p = new Peer();
 		ContactPrivateData privData = p.createContact("alice");
 		ObjectNode out = privData.serializeJSON();
-		
 		ContactPrivateData tmpPrivateData = new ContactPrivateData(out);
 		
 		assertEquals(privData.getId().toString(), tmpPrivateData.getId().toString());
+		assertEquals(privData.getKey().serializeJSON().toString(), tmpPrivateData.getKey().serializeJSON().toString());
+		assertEquals(privData.getParams().serializeJSON().toString(), tmpPrivateData.getParams().serializeJSON().toString());
+	}
+	
+	public void testTwoPeers() {
+		Peer p = new Peer();
+		
+		ContactPrivateData alicePrivData = p.createContact("alice");
+		Peer alice = new Peer();
+		alice.registerContact("p", alicePrivData);
+
+		ContactPrivateData bobPrivData = p.createContact("alice");
+		Peer bob = new Peer();
+		bob.registerContact("p", bobPrivData);
+		
+		alice.addDirectMessage("p", "Attack");
+
+		MessageRequest req = bob.generateRequest("p");
+		
+		System.out.println(req.serializeJSON());
+
 	}
 }

961c9da2a3cf0fc967424898e9d5b448068f3f30
ruchith.fernando <ruchith.fernando@gmail.com>
2014-01-05 18:02:45 +0000

    Send direct messages to a user via the pub channel after adding the user

--- impl/peer/app.js
+++ impl/peer/app.js
@@ -7,11 +7,13 @@ var express = require('express');
 var routes = require('./routes');
 var http = require('http');
 var path = require('path');
+var request = require('request');
 
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 8001);
+var port = process.argv[2];
+app.set('port', process.env.PORT || port);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(express.favicon());
@@ -28,7 +30,28 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
+app.get('/contacts', routes.contacts);
+
+
 app.get('/start', routes.start);
+
+var msg_index = 0;
+var pubchannel_update_interval = 5000;
+//Thread to read the public channel
+setInterval(function() {
+	request('http://localhost:5000/get_all_messages_after?msg_id=' + msg_index, function (error, response, data) {
+		if (!error && response.statusCode == 200) {
+			//Convert to JSON
+			j_data = JSON.parse(data);
+			
+			//Update message index
+			msg_index += j_data.length;
+			routes.process_pub_data(j_data);
+		}
+	});
+}, pubchannel_update_interval);
+
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));

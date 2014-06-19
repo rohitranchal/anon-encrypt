c247eb7b9f398754492b65e722422bcf40b63848
ruchith.fernando <ruchith.fernando@gmail.com>
2014-06-19 23:26:45 +0000

    abolity to return the message counts and closing a message properly

--- impl/pubchannel/app.js
+++ impl/pubchannel/app.js
@@ -15,7 +15,7 @@ app.set('port', process.env.PORT || 5000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(express.favicon());
-app.use(express.logger('dev'));
+// app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
@@ -41,6 +41,7 @@ app.get('/set_message_index_of_peer', routes.set_message_index_of_peer);
 app.post('/direct_message', routes.add_direct_message);
 app.get('/get_direct_messages', routes.get_all_direct_messages_for);
 app.get('/get_all_direct_messages', routes.get_all_direct_messages);
+app.get('/msg_count', routes.msg_count);
 
 
 http.createServer(app).listen(app.get('port'), function(){
